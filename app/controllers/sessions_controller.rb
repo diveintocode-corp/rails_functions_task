80f884a4c8c2fdb8cbe101b49682193c1259ccad
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-03-02 13:19:02 +0900

    [update]国際化対応

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,15 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in(user)
-      redirect_to user_path(user.id), notice: 'ログインしました。'
+      redirect_to user_path(user.id), notice: 'Logged in.'
     else
-      flash.now[:notice] = 'メールアドレスまたはパスワードが間違っています。'
+      flash.now[:notice] = 'Your email address or password is incorrect.'
       render :new
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to new_session_path, notice: 'ログアウトしました。'
+    redirect_to new_session_path, notice: 'Logged out.'
   end
 end
